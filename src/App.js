7701854c5275f6020333ca8f11e0ac96bec545df
riazakhanda017ksn <riazkahanda@gmail.com>
2021-08-03 05:27:52 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+          exercitationem est reiciendis ullam deserunt libero distinctio quia
+          consequatur sint ex, illum, assumenda sit temporibus autem. Tempora
+          laudantium non consequuntur atque!
+        </h1>
       </header>
     </div>
   );
